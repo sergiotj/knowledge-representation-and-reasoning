55ea975125ebb22015d439dfa89edf9497e447d8
Alexandre Pinho <lxpnh98@gmail.com>
2019-03-04 10:23:54 +0000

    Implementar registarServico e registarConsulta.

--- ex1.pl
+++ ex1.pl
@@ -22,12 +22,22 @@ max([X | L], X) :-
     max(L, M),
     X >= M.
 
+solucoes(F, Q, R) :-
+    Q, assert(tmp(F)), fail.
+solucoes(F, Q, R) :-
+    construir([], R).
+
+construir(L, R) :-
+    retract(tmp(X)), !,
+    construir([X | L], R).
+construir(R, R).
+
 % operações
 
 % Registar utentes, serviços e consultas;
 
 nextIdUt(NextId) :-
-    findall(Id, utente(Id, _, _, _), Ids),
+    solucoes(Id, utente(Id, _, _, _), Ids),
     ((max(Ids, MaxId),
      NextId is MaxId + 1);
      Ids = [], % se não houver nenhum utente, o novo id será 1
@@ -37,6 +47,24 @@ registarUtente(Nome, Idade, Cidade) :-
     nao(utente(_, Nome, Idade, Cidade)),
     nextIdUt(NextIdUt),
     assert(utente(NextIdUt, Nome, Idade, Cidade)).
+
+nextIdServ(NextId) :-
+    solucoes(Id, servico(Id, _, _, _), Ids),
+    ((max(Ids, MaxId),
+     NextId is MaxId + 1);
+     Ids = [],
+     NextId is 1).
+
+registarServico(Descricao, Instituicao, Cidade) :-
+    nao(servico(_, Descricao, Instituicao, Cidade)),
+    nextIdServ(NextId),
+    assert(servico(NextId, Descricao, Instituicao, Cidade)).
+
+registarConsulta(Data, IdUt, IdServ, Custo) :-
+    nao(consulta(Data, IdUt, IdServ, Custo)),
+    utente(IdUt, _, _, _),
+    servico(IdServ, _, _, _),
+    assert(consulta(Data, IdUt, IdServ, Custo)).
 
 % Remover utentes, serviços e consultas;
 % Identificar as instituições prestadoras de serviços;
